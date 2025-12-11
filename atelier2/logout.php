d840fbdd6824ebe734f499385d36575ebaa22dde
wartwix <n.sadra1990@gmail.com>
2025-12-11 11:29:53 +0100

    Update logout.php

--- atelier2/logout.php
+++ atelier2/logout.php
@@ -1,8 +1,15 @@
 <?php
-// Supprime le cookie d'authentification
-setcookie('authToken', '', time() - 3600, '/', '', false, true); // Cookie expiré par la valeur -3600. Il n'est donc plus valable
+session_start();
 
-// Redirection vers la page de connexion
-header('Location: index.php');
-exit();
+// 1. Suppression du Cookie (On le met dans le passé pour forcer l'expiration immédiate)
+// setcookie(name, value, expire, path, domain, secure, httponly)
+setcookie('session_token', '', time() - 3600, '/', '', false, true); 
+
+// 2. Destruction de la session PHP côté serveur
+session_unset();  // Supprime les variables de session ($ _SESSION)
+session_destroy(); // Détruit le fichier de session
+
+// 3. Redirection vers la page de connexion
+header('Location: login.php');
+exit;
 ?>
