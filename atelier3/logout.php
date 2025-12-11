7c0a85a8db2cb61fcd526472b150f5fc111f1503
wartwix <n.sadra1990@gmail.com>
2025-12-11 11:50:52 +0100

    Update logout.php

--- atelier3/logout.php
+++ atelier3/logout.php
@@ -1,18 +1,10 @@
 <?php
-// 1. Démarrer la session : Essentiel pour pouvoir manipuler les données et la détruire.
 session_start();
-
-// En-têtes pour prévenir le cache des pages de déconnexion/sécurité (bonne pratique)
-header("Cache-Control: no-cache, must-revalidate"); 
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date passée
-
-// 2. Suppression des variables de session (Ex: 'visit_count', 'username')
-session_unset();
-
-// 3. Destruction de la session PHP côté serveur
+// On détruit toutes les variables de session
+$_SESSION = array();
+// On détruit la session
 session_destroy();
-
-// 4. Rediriger vers la page de connexion
-header('Location: login.php');
+// On redirige vers l'accueil
+header("Location: index.php");
 exit();
 ?>
